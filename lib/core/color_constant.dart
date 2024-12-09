d5a850423a6a4169b64ad57df13531cc6c6d1dcf
humamafif <afifalmahbubi04@gmail.com>
2024-12-09 10:11:05 +0700

    initial commit

---
+++ lib/core/color_constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color black = Colors.black;
+const Color primary = Color.fromRGBO(255, 255, 234, 1);
+const Color secondary = Color.fromARGB(255, 255, 191, 0);
+const Color ashYellow = Color.fromARGB(255, 197, 189, 158);
